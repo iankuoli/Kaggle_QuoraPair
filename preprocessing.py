5358de6ef4a37cbc7a5660efaf789526a3194cf1
IanKuo <iankuoli@gmail.com>
2017-04-13 04:30:50 -0500

    Add function: word_patterns_replace()

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,4 @@
+import re
 import nltk
 from nltk.corpus import wordnet
 from nltk import ne_chunk, pos_tag, word_tokenize
@@ -5,9 +6,61 @@ from nltk.tree import Tree
 nltk.data.path.append('/home/ian/nltk_data')
 
 
+def word_patterns_replace(text):
+    text = text.replace('?', ' ? ')
+    text = text.replace(':', ' : ')
+    text = text.replace(', ', ' , ')
+    text = text.replace('. ', ' . ')
+    text = text.replace('.\"', ' .\"')
+    text = text.replace(',\"\",', ',\" \",')
+    text = text.replace('(', ' ( ')
+    text = text.replace(')', ' ) ')
+    text = text.replace('\'s ', ' \'s ')
+    text = text.replace('s\' ', ' s\' ')
+    text = text.replace('n\'t ', ' not ')
+    text = text.replace('\'m ', ' \'m ')
+
+    # detection symbol or tag
+    text = text.replace('/', ' / ')
+    text = re.sub(r"([\W]) / ([A-Za-z])", r"\1/\2", text)
+    text = text.replace('<$', ' < $')
+    text = re.sub(r"<([0-9])", r"< \1", text)
+
+    #text = re.sub(r"[\W] / [\w]", "/", text)
+
+    # detect brief expression
+    text = re.sub(r'((.[A-Z])+) \.', r'\1.', text)
+
+    # detect unit
+    text = re.sub(r'([0-9])[M,m][H,h][Z,z]', r'\1 mhz', text)
+    text = re.sub(r'([0-9])[H,h][Z,z]', r'\1 hz', text)
+    text = re.sub(r'([0-9])[B,b][P,p][M,m]', r'\1 bpm', text)
+    text = re.sub(r'([0-9])[K,k][M,m] ', r'\1 km ', text)
+    text = re.sub(r'([0-9])[C,c][M,m] ', r'\1 cm ', text)
+    text = re.sub(r'([0-9])[K,k][G,g] ', r'\1 kg ', text)
+    text = re.sub(r'([0-9])[M,m][G,g] ', r'\1 mg ', text)
+    text = re.sub(r'([0-9])[M,m][L,l] ', r'\1 kg ', text)
+    text = re.sub(r'([0-9])[L,l][P,p][A,a] ', r'\1 kg ', text)
+    text = re.sub(r'\$([0-9])', r'$ \1', text)
+    text = re.sub(r'([0-9]) [V,v]', r'\1 volt', text)
+    
+
+    # digit expression
+    text = re.sub(r'([0-9]),([0-9])', r'\1\2', text)
+    text = re.sub(r'([0-9])[K,k] ', r'\g<1>000 ', text)
+    text = re.sub(r'([0-9])\+([0-9])', r'\1 + \2', text)
+
+    text = text.replace('  ', ' ')
+
+    text = tokenizer(text)
+
+    return text
+
+
 def tokenizer(sentence):
     k = len(sentence)
     sentence = sentence.replace("-", "_")
+    sentence = sentence.replace('–', '_')
     list_s = sentence.split(" ")
     for s_idx in range(len(list_s)):
         s = list_s[s_idx]
@@ -50,7 +103,16 @@ def get_continuous_chunks(text):
        continuous_chunk.append(named_entity)
     return continuous_chunk
 
-SS = "I am a 19-year-old man who love state-of-the-art paper and F-14 flight"
-TXT = "Barack Obama is the husband of Michelle Obama"
-print(tokenizer(SS))
-print(get_continuous_chunks(TXT))
+#SS = "I am a 19-year-old man who love neural-based paper and F-14 flight"
+#TXT = "Barack Obama is the husband of Michelle Obama"
+#print(tokenizer(SS))
+#print(get_continuous_chunks(TXT))
+#TXT = "\"Ted's Indian-made <20K 10 V dicks that cost <$10,000 hasn't been detected/protected at (9+2)/11 with [/math] and MOOCs/E-learning (900/1,800 bpm Tu-95).\n PIF: 14-years-old Trump–Clinton U.S. Presidential debate is good for 10Km? <\html>\""
+#TXT = "What is the output for in main {char *ptr=""hello""; ptr [0] ='m'; printf (""%s"" , *s);} ?"
+#TXT = "If light has zero mass , then as per this [math]E=mc^2[/math] , light must have zero energy. Is it so ?"
+#TXT = "What is the story of Kohinoor (Koh-i-Noor) Diamond"
+TXT="How would I find the necessary number of turns on a transformer primary if the secondary voltage required is 120 V at 60 Hz ?"
+TXT="A distribution transformer is rated at 18 kVA , 20,000/480 V , and 60 hz. can this transformer safely supply 15kVA to a 415-V load at 50hz ? Why or not ?"
+print('\nINPUT:\n' + TXT)
+print('\nOUTPUT:\n' + word_patterns_replace(TXT))
+
